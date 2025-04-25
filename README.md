e5ca3ad29fde72bc64d0dac1c14bff41ab8c75dc
Mohit Mundria <mundriamohit100@gmail.com>
2025-04-25 19:00:21 +0530

    Modify app.py and add Procfile and update Readme, requirements.txt files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# End_to_End_Bitcoin_Project
+# End_to_End_AmazonClosingPrice_Prediction
 # Software and Tools Requirements
 
 1. [GithubAccount](https://github.com)
@@ -15,13 +15,25 @@ conda create --name venv python==3.9 -y
 2. git config --global user.email
 
 # Create requirements.txt
-Add all the required modules in requirements.txt
+Add all the required modules in requirements.txt.
 
 # Create app.py and home.html files
-Make code for a web application in the home.html
-   Make sure add home.html in a new folder name "templates"
+Write code that define the logic and functionality of the web application.
+As we know app.py is a simple python file that act the entry point for the Flask.
+Make code for a web application in the home.html.
+   Make sure add home.html in a new folder name "templates".
    
 # Add all the files then commit the changes and then push it to github
 1. git add .
 2. git commit -m "msg"
 3. git push -u origin main
+# Download Heroku Software
+  Download and make account on the Heroku.
+  Heroku is a cloud based software that is used by developers to deploy, build and manage apps.
+  1. Login to Heroku.
+  2. Create new Account.
+  3. Connect account of github.
+  4. Connect repository which you want.
+# Now add Procfile 
+  web:gunicorn app:app
+As we know Procfile is used to give command to Heroku from our system only.
--- app.py
+++ app.py
@@ -8,6 +8,8 @@ scaler=joblib.load(r"D:\End to end project\End_to_End_Bitcoin_Project\StandardSc
 @app.route('/')
 def home():
     return render_template('home.html')
+
+# This is the decorator(function) do the prediction by the help of API. API by Postman Software
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data=request.json['data']
@@ -15,7 +17,21 @@ def predict_api():
     new_data=scaler.transform(numpy.array(list(data.values())).reshape(1,-1))
     output=model.predict(new_data)
     print(output)
-    return jsonify(output[0])
+    
+    #Here we convert the numpy array to regular python type data so that it can convert into jsonify as numpy array can't 
+    # convert directly into json datatype.
+    predicted_price = float(output[0])
+    return jsonify({'prediction_text': f"Predicted Closing Price for is ${predicted_price:.2f}"})
+
+# Now make a decorator(function) that enable us to give our input data on web application only not through API.
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(numpy.array(data).reshape(1,-1))
+    print(final_input)
+    output=model.predict(final_input)[0]
+    return render_template("home.html", prediction_text="The Closing price is {}".format(output))
+
 if __name__=="__main__":
     app.run(debug=True)
     
