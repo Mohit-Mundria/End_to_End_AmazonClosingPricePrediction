89865a45e99872ab52cef94a28cedc98cab5d217
Mohit-Mundria <mundriamohit100@gmail.com>
2025-04-26 12:54:13 +0530

    Update app.py
    Fix error while deploying

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, request, app, jsonify, url_for, render_template, redire
 import pandas
 import numpy
 app=Flask(__name__)
-model=joblib.load(r"D:\End to end project\End_to_End_Bitcoin_Project\Deep_learning_Stock_Prediction1",'rb')
-scaler=joblib.load(r"D:\End to end project\End_to_End_Bitcoin_Project\StandardScaler1")
+model=joblib.load("Deep_learning_Stock_Prediction1")
+scaler=joblib.load("StandardScaler1")
 @app.route('/')
 def home():
     return render_template('home.html')
